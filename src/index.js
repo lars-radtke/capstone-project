d77bb1db0158d012a0fecbecdcc10015d4c34fb6
Lars Radtke <lars.radtke@gmx.net>
2021-10-18 20:27:29 +0200

    import GlobalStyles

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from 'components';
 import reportWebVitals from './reportWebVitals';
+import GlobalStyles from './Styles/GlobalStyles';
 
 ReactDOM.render(
     <React.StrictMode>
+        <GlobalStyles />
         <App />
     </React.StrictMode>,
     document.getElementById('root'),
