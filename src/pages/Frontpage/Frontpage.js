d6dd85997b3d8d20e1c40eb52165154ecae6b9fd
Lars Radtke <lars.radtke@gmx.net>
2021-10-18 20:59:13 +0200

    import Logo to Frontpage

--- src/pages/Frontpage/Frontpage.js
+++ src/pages/Frontpage/Frontpage.js
@@ -1,3 +1,19 @@
+import { Logo } from 'components';
+import styled from 'styled-components/macro';
+
 export const Frontpage = () => {
-    return <>Frontpage</>;
+    return (
+        <Page>
+            <Logo />
+        </Page>
+    );
 };
+
+const Page = styled.section`
+    height: 100%;
+    padding: 50px;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
