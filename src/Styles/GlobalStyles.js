c05108a33f13bfaf3b0246078973ca11f31a872e
Lars Radtke <lars.radtke@gmx.net>
2021-10-18 20:33:06 +0200

    add additional rules to GlobalStyles

--- src/Styles/GlobalStyles.js
+++ src/Styles/GlobalStyles.js
@@ -2,15 +2,29 @@ import { createGlobalStyle } from 'styled-components';
 import './Fonts/Montserrat.css';
 
 const GlobalStyles = createGlobalStyle`
+	:root {
+		--white: rgba(255,255,255,1);
+		--grey: rgba(210,210,210,1);
+		--black: rgba(15,15,15,1);
+		--red: rgba(209,0,38,1);
+	}
+
 	* {
 		box-sizing: border-box;
 		margin: 0;
 		padding: 0;
 		line-height: 1.5;
+		font-family: 'Montserrat', Arial, Helvetica, sans-serif;
 	}
 
 	body {
 		height: 100vh;
+
+		color: var(--black);
+
+		background-image: linear-gradient(rgb(218,218,218) 0%, rgb(255,255,255) 100%);
+		background-repeat: no-repeat;
+
 		scroll-behavior: smooth;
 	}
 
