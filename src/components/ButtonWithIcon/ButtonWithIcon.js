29b7f63c04c7a0fd6a66d89005c8c8c91e1cccfa
Lars Radtke <lars.radtke@gmx.net>
2021-10-18 22:49:07 +0200

    create Button component

---
+++ src/components/ButtonWithIcon/ButtonWithIcon.js
@@ -0,0 +1,37 @@
+import styled from 'styled-components/macro';
+
+export const ButtonWithIcon = ({
+    icon,
+    text,
+    highlightend,
+    disabled,
+    action,
+}) => {
+    return (
+        <ButtonSpacer>
+            <Click
+                highlightend={highlightend}
+                disabled={disabled}
+                onClick={action}
+            >
+                <img src={icon} alt="" />
+                {text}
+            </Click>
+        </ButtonSpacer>
+    );
+};
+
+const ButtonSpacer = styled.div`
+    display: inline-block;
+
+    padding-top: 24px;
+`;
+
+const Click = styled.button`
+    padding: 10px 20px 10px 18px;
+
+    display: grid;
+    grid-template-columns: 24px auto;
+    gap: 18px;
+    align-items: center;
+`;
