2b714008569789386ccb7867644ae53a9fe9b3d2
Lars Radtke <lars.radtke@gmx.net>
2021-10-18 22:49:48 +0200

    import Global Styles to storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,20 @@
+import GlobalStyles from '../src/Styles/GlobalStyles';
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+        matchers: {
+            color: /(background|color)$/i,
+            date: /Date$/,
+        },
     },
-  },
-}
+};
+
+export const decorators = [
+    Story => (
+        <>
+            <GlobalStyles />
+            <Story />
+        </>
+    ),
+];
