16290e44829bbeecb9c6439b7d506ded2184cf30
Lars Radtke <lars.radtke@gmx.net>
2021-10-20 16:13:21 +0200

    update Login component based on extractions

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,75 +1,89 @@
 import styled from 'styled-components/macro';
-import { ButtonWithIcon } from 'components';
+import { useEffect, useState } from 'react';
+import { TextInput, ButtonWithIcon, PasswordInput } from 'components';
 
-export const Login = onSubmit => {
-    function login(event) {
-        event.preventDefault();
-        const form = event.target;
-        const name = form.name.value;
-        if (name.indexOf('.') !== -1 && name.indexOf('@') !== -1) {
-            onSubmit(name);
-            form.reset();
+export const Login = ({ loginHandler }) => {
+    const dataNotFound = false;
+    const [nameFilled, setNameFilled] = useState(false);
+    const [nameWrongFormat, setNameWrongFormat] = useState(false);
+    const [passwordWrongLength, setPasswordWrongLength] = useState(false);
+    const [passwordFilled, setPasswordFilled] = useState(false);
+    const [inactive, setInactive] = useState(true);
+
+    const focusOutName = event => {
+        const input = event.target.value;
+        if (
+            input &&
+            input.indexOf('.') !== -1 &&
+            input.indexOf('@') !== -1 &&
+            input.indexOf('@') > input.indexOf('.') + 1 &&
+            input.indexOf('@') + 1 < input.length
+        ) {
+            setNameFilled(true);
+            setNameWrongFormat(false);
+        } else {
+            setNameFilled(false);
+            setNameWrongFormat(true);
         }
-    }
+    };
+
+    const focusOutPassword = event => {
+        const input = event.target.value;
+        if (input.length >= 6) {
+            setPasswordFilled(true);
+            setPasswordWrongLength(false);
+        } else {
+            setPasswordFilled(false);
+            setPasswordWrongLength(true);
+        }
+    };
+
+    useEffect(() => {
+        if (nameFilled === true && passwordFilled === true) {
+            setInactive(false);
+        } else {
+            setInactive(true);
+        }
+    }, [passwordFilled, nameFilled]);
 
     return (
-        <Form onSubmit={event => login(event)}>
-            <StyledLabel>
-                Benutzername
-                <StyledInput
-                    type="text"
+        <>
+            <Form onSubmit={event => loginHandler(event)}>
+                <TextInput
+                    label="Benutzername"
                     name="name"
-                    placeholder="max.musterschueler@class"
-                    autoComplete="off"
-                    required
+                    placeholder="max.musterschueler@klasse"
+                    focusOut={focusOutName}
+                    dataNotFound={dataNotFound}
+                    error={nameWrongFormat}
+                    errorText="Deine Eingabe entspricht nicht dem gültigen Format."
+                    helpText="Beispiel: max.musterschueler@klasse"
                 />
-            </StyledLabel>
-            <StyledLabel>
-                Passwort
-                <StyledInput
+                <PasswordInput
+                    label="Passwort"
                     type="password"
                     name="password"
                     placeholder="SuperSicheresPasswort"
-                    autoComplete="off"
-                    required
+                    focusOut={focusOutPassword}
+                    dataNotFound={dataNotFound}
+                    error={passwordWrongLength}
+                    errorText="Deine Eingabe entspricht nicht der gültigen Mindestlänge."
+                    helpText="Das Passwort muss aus mindestens 6 Zeichen bestehen."
                 />
-            </StyledLabel>
+            </Form>
             <Wrapper>
                 <ButtonWithIcon
                     icon="/assets/icons/login.svg"
                     text="Anmelden"
+                    inactive={inactive}
                 />
             </Wrapper>
-        </Form>
+        </>
     );
 };
 
 const Form = styled.form`
     width: 100%;
-`;
-
-const StyledLabel = styled.label`
-    width: 100%;
-    font-size: 14px;
-    font-weight: 100;
-    display: block;
-    &:not(:last-of-type) {
-        margin-bottom: 25px;
-    }
-    border-bottom: 1px solid var(--black);
-`;
-const StyledInput = styled.input`
-    display: block;
-    width: 100%;
-    padding: 10px 0;
-    border: none;
-    background: none;
-    font-size: 16px;
-    font-weight: 500;
-    &::placeholder {
-        font-weight: 200;
-        opacity: 0.5;
-    }
 `;
 
 const Wrapper = styled.div`
