60f77cae2a8cd1a8cd774f3ee37cb818f30ce47f
Lars Radtke <lars.radtke@gmx.net>
2021-10-22 19:28:40 +0200

    create user authentication

--- src/pages/Frontpage/Frontpage.js
+++ src/pages/Frontpage/Frontpage.js
@@ -1,11 +1,32 @@
 import { Logo, Login } from 'components';
+import data from '../../data.json';
 import styled from 'styled-components/macro';
+import { useState } from 'react';
 
-export const Frontpage = onSubmit => {
+export const Frontpage = ({ authenticateUser }) => {
+    const [dataNotFound, setDataNotFound] = useState(false);
+
+    const handleLogin = event => {
+        event.preventDefault();
+        const { name, password } = event.target.elements;
+
+        const user = data.filter(
+            user =>
+                user.login === name.value && user.password === password.value,
+        );
+
+        if (user.length !== 0) {
+            setDataNotFound(false);
+            authenticateUser(user);
+        } else {
+            setDataNotFound(true);
+        }
+    };
+
     return (
         <Page>
             <Logo />
-            <Login onSubmit={onSubmit} />
+            <Login handleLogin={handleLogin} dataNotFound={dataNotFound} />
         </Page>
     );
 };
