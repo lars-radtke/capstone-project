91ddd063c3f85291fe6c8fd3570bb6eec407869e
Lars Radtke <lars.radtke@gmx.net>
2021-10-23 03:04:52 +0200

    add user authentication and actual login redirect

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,43 @@
-import { Frontpage } from 'pages';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import { Frontpage, Dashboard } from 'pages';
+import { useState } from 'react';
 
 export const App = () => {
-    return <Frontpage />;
+    const [user, setUser] = useState(() => {
+        if (localStorage.getItem('user')) {
+            return JSON.parse(localStorage.getItem('user'));
+        } else return [];
+    });
+
+    const userIsAuthenticated = user.length !== 0;
+
+    const authenticateUser = array => {
+        localStorage.setItem('user', JSON.stringify(array));
+        setUser(array);
+    };
+
+    const logoutUser = () => {
+        localStorage.removeItem('user');
+        setUser([]);
+        document.title = 'Anmelden | Student Compass';
+    };
+
+    return (
+        <Switch>
+            <Route exact path="/">
+                {userIsAuthenticated ? (
+                    <Redirect to="/dashboard" />
+                ) : (
+                    <Frontpage authenticateUser={authenticateUser} />
+                )}
+            </Route>
+            <Route path="/dashboard">
+                {userIsAuthenticated ? (
+                    <Dashboard user={user} logoutUser={logoutUser} />
+                ) : (
+                    <Redirect to="/" />
+                )}
+            </Route>
+        </Switch>
+    );
 };
