7f1e58da9ed3d01edc177db25311809558d58907
Lars Radtke <lars.radtke@gmx.net>
2021-10-27 10:51:04 +0200

    import the useInterval hook

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,33 @@
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { Frontpage, Dashboard } from 'pages';
 import { useState } from 'react';
+import { useInterval } from 'hooks/useInterval';
+import { useBackgroundColor } from 'hooks';
 
 export const App = () => {
+    const initialDate = new Date();
+    const [day, setDay] = useState(initialDate.getDay());
+    const [hours, setHours] = useState(initialDate.getHours());
+    const [minutes, setMinutes] = useState(initialDate.getMinutes());
+
+    // This is a custom function, that mocks the setInterval function.
+    // As setInterval() does not work in React as you would think, the use of a custom hook was necessary.
+    // It tests for changes every second, but only updates states when a change is detected
+    useInterval(() => {
+        const newDate = new Date();
+        if (newDate.getDay() !== day) {
+            setDay(newDate.getDay());
+        }
+        if (newDate.getHours() !== hours) {
+            setHours(newDate.getHours());
+        }
+        if (newDate.getMinutes() !== minutes) {
+            setMinutes(newDate.getMinutes());
+        }
+    }, 1000);
+
+    useBackgroundColor(hours);
+
     const [user, setUser] = useState(() => {
         if (localStorage.getItem('user')) {
             return JSON.parse(localStorage.getItem('user'));
@@ -33,7 +58,12 @@ export const App = () => {
             </Route>
             <Route path="/dashboard">
                 {userIsAuthenticated ? (
-                    <Dashboard user={user} logoutUser={logoutUser} />
+                    <Dashboard
+                        day={day}
+                        user={user}
+                        hours={hours}
+                        logoutUser={logoutUser}
+                    />
                 ) : (
                     <Redirect to="/" />
                 )}
