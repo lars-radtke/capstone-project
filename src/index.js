9ad773774df8261efabcd79f94a789e18bc1de4f
Lars Radtke <lars.radtke@gmx.net>
2021-11-02 00:59:09 +0100

    yet another try to fix

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { App } from 'components';
 import reportWebVitals from './reportWebVitals';
-import GlobalStyles from 'styles/GlobalStyles';
+import GlobalStyles from 'src/styles/GlobalStyles';
 
 ReactDOM.render(
     <React.StrictMode>
