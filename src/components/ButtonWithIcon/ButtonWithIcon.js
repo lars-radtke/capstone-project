15fdcae1aeff0b135015f6520fb9af4de1a15715
Lars Radtke <lars.radtke@gmx.net>
2021-11-02 00:48:43 +0100

    update icon button

--- src/components/ButtonWithIcon/ButtonWithIcon.js
+++ src/components/ButtonWithIcon/ButtonWithIcon.js
@@ -16,15 +16,39 @@ export const ButtonWithIcon = ({
 };
 
 const Button = styled.button`
-    padding: 10px 20px 10px 18px;
-    margin-top: 18px;
-    border-radius: 20px;
+    width: 100%;
+    padding: 10px 0;
 
     display: grid;
     grid-template-columns: 24px auto;
     gap: 18px;
+    justify-content: center;
     align-items: center;
 
-    opacity: ${({ disabled }) => (disabled ? '0.3' : '1')};
+    border: none;
+    border-radius: 10px;
+
+    color: ${({ highlighted }) =>
+        highlighted ? 'var(--white)' : 'var(--black)'};
+    font-size: 16px;
+    font-weight: 500;
+    white-space: nowrap;
+
+    transition: 0.2s;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3);
+
+    background-color: ${({ highlighted }) =>
+        highlighted ? 'var(--red)' : 'var(--white)'};
+    opacity: ${({ disabled }) => (disabled ? '0.2' : '1')};
     pointer-events: ${({ disabled }) => (disabled ? 'none' : 'auto')};
+
+    img {
+        filter: ${({ highlighted }) =>
+            highlighted ? 'invert(1)' : 'invert(0)'};
+    }
+
+    :active {
+        transform: translateY(8px);
+        box-shadow: 0 0 0 rgba(0, 0, 0, 0);
+    }
 `;
